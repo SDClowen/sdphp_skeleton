813e2a2652b9b625d14524ebe770707b2334d6a3
Mahmut YILDIRIM <mahmutyildirim@outlook.com.tr>
2025-08-17 22:31:10 +0300

    changed app run func name

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 require_once "vendor/autoload.php";
 
-$elapsedTime = \Core\App::weakup(__DIR__);
+$elapsedTime = \Core\App::wakeup(__DIR__);
 stackMessages("Page loaded in: $elapsedTime s", true);
